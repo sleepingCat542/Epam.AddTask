b77698f4de85295f290ebe234dfb46556ceb3fab
Yuliana <yuki.sai@mail.ru>
2019-10-07 01:41:51 +0300

    add project with NorthwindDataService.cs

---
+++ Task2/AppForNorthwindDataService/AppForNorthwindDataService/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForNorthwindDataService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NorthwindModel.NorthwindEntities entities = new NorthwindModel.NorthwindEntities(new Uri("https://services.odata.org/V3/Northwind/Northwind.svc"));
+            var customers = entities.Customers.ToArray();
+            Console.WriteLine("{0} customers in the service found.", customers.Length);
+        }
+    }
+}
